b9ca59252c6e9317a64d001dd70bd974c42377ba
Dmitrii Kim <mitya_kim@icloud.com>
2024-05-01 21:57:29 -0700

    Add subscription

--- WeatherApp/WeatherApp/Controllers/WeatherViewController.swift
+++ WeatherApp/WeatherApp/Controllers/WeatherViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RevenueCatUI
+import RevenueCat
+import WeatherKit
 
 class WeatherViewController: UIViewController {
     
@@ -26,7 +29,7 @@ extension WeatherViewController {
         view.backgroundColor = .systemBackground
         primaryView.translatesAutoresizingMaskIntoConstraints = false
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"), style: .done, target: self, action: #selector(didTapUpgrade))
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"), style: .done, target: self, action: #selector(didTapUpgrade))
     }
     
     private func layout() {
@@ -45,18 +48,50 @@ extension WeatherViewController {
             WeatherManager.shared.getWeather(for: location) { [weak self] in
                 DispatchQueue.main.async {
                     guard let currentWeather = WeatherManager.shared.currentWeather else { return }
-                    self?.primaryView.configure(with: [
-                        .current(viewModel: .init(model: currentWeather)),
-                        .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0 ) })),
-                        .daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0 ) }))
-                    ])
+                    self?.createViewModels(currentWeather: currentWeather)
+                }
+            }
+        }
+    }
+    
+    private func createViewModels(currentWeather: CurrentWeather) {
+        var viewModels: [WeatherViewModel] = [
+            .current(viewModel: .init(model: currentWeather)),
+            .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0 ) })),
+        ]
+        
+        primaryView.configure(with: viewModels)
+        
+        IAPManager.shared.isSubscribed { [weak self] subscribed in
+            DispatchQueue.main.async {
+                if subscribed {
+                    viewModels.append(.daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0 ) })))
+                    self?.primaryView.configure(with: viewModels)
+                } else {
+                    self?.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"), style: .done, target: self, action: #selector(self?.didTapUpgrade))
                 }
             }
         }
     }
     
     @objc private func didTapUpgrade() {
-        print("Hello")
+        let vc = PaywallViewController()
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+}
+
+extension WeatherViewController: PaywallViewControllerDelegate {
+    func paywallViewController(_ controller: PaywallViewController, didFinishPurchasingWith customerInfo: CustomerInfo) {
+//        print("Purchased: \(customerInfo)")
+        controller.dismiss(animated: true)
+        guard let currentWeather = WeatherManager.shared.currentWeather else { return }
+        createViewModels(currentWeather: currentWeather)
+    }
+    
+    func paywallViewController(_ controller: PaywallViewController, didFinishRestoringWith customerInfo: CustomerInfo) {
+//        print("Purchased: \(customerInfo)")
+        controller.dismiss(animated: true)
     }
 }
 
--- WeatherApp/WeatherApp/Managers/IAPManager.swift
+++ WeatherApp/WeatherApp/Managers/IAPManager.swift
@@ -5,6 +5,7 @@
 //  Created by Mitya Kim on 4/28/24.
 //
 
+import RevenueCat
 import Foundation
 
 
@@ -12,4 +13,11 @@ final class IAPManager {
     static let shared = IAPManager()
     
     private init () {}
+    
+    func isSubscribed(completion: @escaping (Bool) -> Void) {
+        Purchases.shared.getCustomerInfo { info, error in
+            guard let subscriptions = info?.activeSubscriptions else { return }
+            completion(!subscriptions.isEmpty)
+        }
+    }
 }
